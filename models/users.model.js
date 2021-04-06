2685a33c6b6fbc5eb7f443acf46fcda4fdce84b8
phucloi97 <phucloi97@hotmail.com>
2021-04-06 20:30:38 +0700

    test

---
+++ models/users.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+// var validateEmail = function (email) {
+//   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   return re.test(email);
+// };
+
+// var EmailSchema = new Schema({
+//   email: {
+//     type: String,
+//     trim: true,
+//     lowercase: true,
+//     unique: true,
+//     required: "Email address is required",
+//     validate: [validateEmail, "Please fill a valid email address"],
+//     match: [
+//       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+//       "Please fill a valid email address",
+//     ],
+//   },
+// });
